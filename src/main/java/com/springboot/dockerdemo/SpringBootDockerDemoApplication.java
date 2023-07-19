1f828627f153ccbe44b09809cd8f48d6864b37c1
Sivabalarasu P <sivabalarasu99@gmail.com>
2023-07-19 11:46:52 +0530

    Added docker demo files

---
+++ src/main/java/com/springboot/dockerdemo/SpringBootDockerDemoApplication.java
@@ -0,0 +1,22 @@
+package com.springboot.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerDemoApplication {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerDemoApplication.class, args);
+	}
+
+}
